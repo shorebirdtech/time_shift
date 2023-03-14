492ebd07879b76a97663534d16516a2af7ef992f
Felix Angelov <felix@shorebird.dev>
2023-03-14 16:34:03 -0500

    feat: immersive sticky ui mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:_shorebird_clock/clocks/particle_clock/particle_clock.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const ParticleClock());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(const ParticleClock());
+}
