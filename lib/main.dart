77393756e1f586ee10eff572d5c72bf4a622168f
Bryan Oltman <bryanoltman@gmail.com>
2023-11-01 12:22:38 -0400

    feat: add perf overlay option (#14)
    * feat: add perf overlay option
    
    * Update dart-define examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:time_shift/update_button.dart';
 
 import 'clocks/clock_face.dart';
 
@@ -8,8 +7,14 @@ import 'clocks/clock_face.dart';
 /// provided or if the value provided is unrecognized.
 ///
 /// Example:
-/// `shorebird run -- --dart-define clock_face=generative`
+/// `shorebird run -- --dart-define=clock_face=generative`
 const clockFaceArgName = 'clock_face';
+
+/// Use this argument to show the performance overlay.
+///
+/// Example:
+/// `shorebird run -- --dart-define=show_perf_overlay=true`
+const showPerfOverlayArgName = 'show_perf_overlay';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +28,8 @@ void main() {
 
   runApp(
     MaterialApp(
+      showPerformanceOverlay:
+          const bool.fromEnvironment(showPerfOverlayArgName),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: clock.widget,
